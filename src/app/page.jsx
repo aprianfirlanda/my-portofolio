995df5c457618abc50c2bd1e7bd06b774f72644b
aprianfirlanda <aprianfirlanda@gmail.com>
2024-05-19 13:04:17 +0700

    [home page - navbar] complete mobile and desktop view

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,78 @@
+"use client"
+
 import Image from "next/image";
+import {useEffect} from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector("header");
+      const fixedNav = header.offsetTop;
+
+      if (window.scrollY > fixedNav) {
+        header.classList.add("navbar-fixed");
+      } else {
+        header.classList.remove("navbar-fixed");
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const toggleHamburger = () => {
+    const hamburger = document.querySelector("#hamburger")
+    const navMenu = document.querySelector("#nav-menu")
+
+    hamburger.classList.toggle("hamburger-active");
+    navMenu.classList.toggle("hidden");
+  }
+
   return (
     <>
+      <header className="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+        <div className="container">
+          <div className="flex items-center justify-between relative">
+            <div className="px-4">
+              <a href="#home" className="font-bold text-lg text-primary block py-6">Aprian Firlanda</a>
+            </div>
+            <div className="flex items-center px-4">
+              <button id="hamburger" name="hamburger" type="button" className="block absolute right-4 lg:hidden"
+                      onClick={toggleHamburger}>
+                <span className="hamburger-line origin-top-left"></span>
+                <span className="hamburger-line"></span>
+                <span className="hamburger-line origin-bottom-left"></span>
+              </button>
+              <nav id="nav-menu" className="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                <ul className="block lg:flex">
+                  <li className="group">
+                    <a href="#home" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Beranda</a>
+                  </li>
+                  <li className="group">
+                    <a href="#about" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Tentang Saya</a>
+                  </li>
+                  <li className="group">
+                    <a href="#portfolio" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Portfolio</a>
+                  </li>
+                  <li className="group">
+                    <a href="#clients" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Clients</a>
+                  </li>
+                  <li className="group">
+                    <a href="#blog" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Blog</a>
+                  </li>
+                  <li className="group">
+                    <a href="#contact" className="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Contact</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
       <section id="home" className="pt-36">
         <div className="container">
           <div className="flex flex-wrap">
