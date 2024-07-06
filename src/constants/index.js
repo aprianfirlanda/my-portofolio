53359a572a6fafd906f92d883d753aa3bbbc40d8
aprianfirlanda <aprianfirlanda@gmail.com>
2024-07-06 21:50:50 +0700

    rc: split code header and hamburger menu

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const navItems = [
+  {
+    link: '#home',
+    name: 'Beranda'
+  },
+  {
+    link: '#about',
+    name: 'Tentang Saya'
+  },
+  {
+    link: '#portfolio',
+    name: 'Portfolio'
+  },
+  {
+    link: '#clients',
+    name: 'Clients'
+  },
+  {
+    link: '#blog',
+    name: 'Blog'
+  },
+  {
+    link: '#contact',
+    name: 'Contact'
+  }
+]
