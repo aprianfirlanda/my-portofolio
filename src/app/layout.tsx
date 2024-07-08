6c75478d06de64b67e97254983bb09fbcfa42c1b
aprianfirlanda <aprianfirlanda@gmail.com>
2024-07-08 18:17:29 +0700

    feat: update metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Aprian Firlanda Imani',
-  description: 'This is My Portfolio as Full Stack Web Developer'
+  description:
+    'This is My Portfolio as Full Stack Web Developer. ' +
+    'I have over 4 years of experience as a web developer, creating ' +
+    'robust and scalable web applications. Skilled in both front-end ' +
+    'and back-end development, I ensure seamless functionality. ' +
+    'Proficient in various languages and frameworks, I thrive in ' +
+    'collaborative environments. Spring Boot | Angular | Next.js ' +
+    '| React.js | PostgreSQL | Docker | Node.js | Redis'
 }
 
 export default function RootLayout({
