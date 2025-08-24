138d3b6ff67fb449b337cbbf34e00dadde83d06f
aprianfirlanda <aprianfirlanda@gmail.com>
2025-08-25 06:19:50 +0700

    fix(HomeHero): update "Contact me" link to redirect to WhatsApp with prefilled phone number

--- src/features/home/components/HomeHero.jsx
+++ src/features/home/components/HomeHero.jsx
@@ -30,7 +30,7 @@ function HomeHero() {
                 </p>
                 <div className="mt-10 flex items-center gap-x-6">
                   <Link
-                    to="/"
+                    to="https://api.whatsapp.com/send/?phone=%2B6282253600917&text&type=phone_number&app_absent=0"
                     className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     Contact me
