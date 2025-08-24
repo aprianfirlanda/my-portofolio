2837a5e9c147fe7ec9e32e846a39fd2a21195e58
aprianfirlanda <aprianfirlanda@gmail.com>
2025-08-25 06:17:02 +0700

    fix(Header): enhance sticky header behavior by adding `top-0 z-10` styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header({ navigation }) {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="sticky bg-white">
+    <header className="sticky top-0 z-10 bg-white">
       <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
